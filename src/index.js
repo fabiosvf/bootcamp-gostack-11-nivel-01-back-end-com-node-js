00f42190314eee9ed0f0aba45b7dac23b38e636f
Fabio Silva Ferreira <fabiosvf@gmail.com>
2020-07-02 23:10:24 -0300

    Nivel 01 - Back-end com Node.js - Middlewares - Criando middleware de validação

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = new express();
 
@@ -35,9 +35,21 @@ function logRequests(request, response, next) {
 
   const logLabel = `[${method.toUpperCase()}] ${url}`;
 
-  console.log(logLabel);
+  console.time(logLabel);
 
   next(); // Próximo middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project ID." });
+  }
+
+  next();
 }
 
 app.use(logRequests);
@@ -61,7 +73,7 @@ app.post("/projects", (request, response) => {
   return response.json(project);
 });
 
-app.put("/projects/:id", (request, response) => {
+app.put("/projects/:id", validateProjectId, (request, response) => {
   const { id } = request.params;
   const { title, owner } = request.body;
 
@@ -82,7 +94,7 @@ app.put("/projects/:id", (request, response) => {
   return response.json(project);
 });
 
-app.delete("/projects/:id", (request, response) => {
+app.delete("/projects/:id", validateProjectId, (request, response) => {
   const { id } = request.params;
 
   const projectIndex = projects.findIndex((project) => project.id === id);
